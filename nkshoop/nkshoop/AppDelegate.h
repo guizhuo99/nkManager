51eb5a4faa791583f9cbade10d5d3fee5db464de
仇凯 <9k@choukaideimac.local>
2016-07-19 12:17:10 -0700

    新建

---
+++ nkshoop/nkshoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nkshoop
+//
+//  Created by 仇凯 on 16/7/19.
+//  Copyright © 2016年 仇凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nkshoop/nkshoop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nkshoop
+//
+//  Created by 仇凯 on 16/7/19.
+//  Copyright © 2016年 仇凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
